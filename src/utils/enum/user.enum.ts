5d6251d313990ac7defbb4b1b4912c2575d1b291
abdulazizkhazratkulovunical <abdulazizkhazratkulov@unical.uz>
2025-08-11 16:12:19 +0500

    feat: add language and user role enums for improved user management

---
+++ src/utils/enum/user.enum.ts
@@ -0,0 +1,11 @@
+export enum language {
+  UZ = 'uz',
+  RU = 'ru',
+}
+
+export enum UserRoleEnum {
+  SUPER_ADMIN = 'super_admin',
+  ADMIN = 'admin',
+  CLIENT = 'client',
+  OPERATOR = 'operator',
+}
