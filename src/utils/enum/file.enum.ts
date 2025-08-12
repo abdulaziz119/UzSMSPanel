b6a7d8f3dfb2cb973a4da7c0328139392b20ddac
abdulazizkhazratkulovunical <abdulazizkhazratkulov@unical.uz>
2025-08-12 11:19:27 +0500

    feat: update FileCategory enum to replace specific categories with passport and address

--- src/utils/enum/file.enum.ts
+++ src/utils/enum/file.enum.ts
@@ -14,15 +14,7 @@ export enum FileVisibility {
 }
 
 export enum FileCategory {
-  PROFILE_AVATAR = 'profile_avatar',
-  PRODUCT_IMAGE = 'product_image',
-  PRODUCT_GALLERY = 'product_gallery',
-  BANNER_IMAGE = 'banner_image',
-  DELIVERY_POINT = 'delivery_point',
-  BRAND_LOGO = 'brand_logo',
-  CATEGORY_IMAGE = 'category_image',
-  DOCUMENT_UPLOAD = 'document_upload',
-  CHAT_ATTACHMENT = 'chat_attachment',
-  TEMP_UPLOAD = 'temp_upload',
+  PASSPORT = 'passport',
+  ADDRESS = 'address',
   OTHER = 'other',
 }
