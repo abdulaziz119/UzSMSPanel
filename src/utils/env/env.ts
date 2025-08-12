2dd7a03092779f159c7dd7b8963d2a3ffc037832
abdulazizkhazratkulovunical <abdulazizkhazratkulov@unical.uz>
2025-08-12 18:54:58 +0500

    feat: remove SMTP_USER and SMTP_PASS environment variables

--- src/utils/env/env.ts
+++ src/utils/env/env.ts
@@ -13,9 +13,6 @@ const SERVER_BASE_URL = process.env.SERVER_BASE_URL || '';
 
 const JWT_SECRET = process.env.JWT_SECRET || '';
 
-const SMTP_USER = process.env.SMTP_USER || '';
-const SMTP_PASS = process.env.SMTP_PASS || '';
-
 // Port konfiguratsiyasi
 const DASHBOARD_PORT = process.env.DASHBOARD_PORT || 8080;
 const FRONTEND_PORT = process.env.FRONTEND_PORT || 3000;
@@ -27,8 +24,6 @@ if (!DB_SCHEMA || !DB_HOST || !DB_USER || !DB_DB || !DB_PASS) {
 export {
   MEDIA_DIRECTORY,
   SERVER_BASE_URL,
-  SMTP_USER,
-  SMTP_PASS,
   DASHBOARD_PORT,
   DB_DB,
   DB_HOST,
