e9c081161525e9058fa1899d2caac5f999608f8e
abdulazizkhazratkulovunical <abdulazizkhazratkulov@unical.uz>
2025-08-12 14:44:53 +0500

    feat: add status field to SmsContact entity with SMSContactStatusEnum

--- src/entity/sms-contact.entity.ts
+++ src/entity/sms-contact.entity.ts
@@ -6,12 +6,20 @@ import {
 } from './base.entity';
 import { DB_SCHEMA } from '../utils/env/env';
 import { SmsGroupEntity } from './sms-group.entity';
+import { SMSContactStatusEnum } from '../utils/enum/sms-contact.enum';
 
 @Entity({ schema: DB_SCHEMA, name: 'sms_contacts' })
 @Index(['phone'])
 export class SmsContactEntity extends BaseEntity {
   @Column({ type: 'varchar', nullable: true })
   name: string | null;
+
+  @Column({
+    type: 'enum',
+    enum: SMSContactStatusEnum,
+    default: SMSContactStatusEnum.APPROVED,
+  })
+  status: SMSContactStatusEnum;
 
   @Column({ type: 'varchar', length: 20, nullable: false })
   phone: string;
---
+++ src/utils/enum/sms-contact.enum.ts
@@ -0,0 +1,5 @@
+export enum SMSContactStatusEnum {
+  APPROVED = 'approved',
+  INVALID_FORMAT = 'invalid_format',
+  BANNED_NUMBER = 'banned_number',
+}
